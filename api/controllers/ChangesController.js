5500cae69acc047e1d322e23e0bb4c82a377c9c8
Roman Hromenkov <roman.hromenkov@gmail.com>
2016-10-14 12:45:07 +0300

    Created new Objects: Changes, Payments

---
+++ api/controllers/ChangesController.js
@@ -0,0 +1,9 @@
+/**
+ * ChangesController
+ *
+ * @description :: Server-side logic for managing changes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {};
+
---
+++ api/models/Payments.js
@@ -0,0 +1,17 @@
+/**
+ * Payments.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+    attributes: {
+        cost: {
+            type: 'integr',
+            requred: true
+        }
+    }
+};
+
